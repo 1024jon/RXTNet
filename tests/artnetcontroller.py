2eb53d695922c8d3cb9dbe8721e496d005becaed
1024jon <1024jon@gmail.com>
2021-03-23 22:12:14 +0000

    file cleanup

---
+++ tests/artnetcontroller.py
@@ -0,0 +1,4 @@
+import sys
+
+sys.path.append("/home/server/git/RXTNet/xled")
+import xled
--- xled/build/lib/xled/Twinkly.py
+++
@@ -1,6 +0,0 @@
-
-import xled
-from requests.compat import urljoin
-
-testcontroller = xled.ControlInterface('192.168.3.218', '98:f4:ab:36:ae:39')
-testcontroller.set_mode('movie')
