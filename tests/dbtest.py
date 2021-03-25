cd641ab976ff408056165dc6898953dedfd2d64e
1024jon <1024jon@gmail.com>
2021-03-25 23:57:07 +0000

    working db test

--- tests/dbtest.py
+++ tests/dbtest.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("/home/server/git/RXTNet/xled")
 import xled
 import mariadb
+from contextlib import suppress
 
 # Connect to MariaDB Platform
 try:
@@ -19,17 +20,24 @@ except mariadb.Error as e:
     sys.exit(1)
 
 cur = conn.cursor()
+controllerlist = []
 #try:
     #cur.execute("INSERT INTO rxtnet.Riverside(Name, MacAddress, IP, NumLEDS, ChannelsPerLED, StartChannel) VALUES (?, ?, ?, ?, ?, ?)",('Twinkly1', 'ddddddd', '192.168.0.8', '250', '3', '(0,0)'))
     #conn.commit()
 #except mariadb.Error as e:
         #print(f"Error: {e}")
-controllers = xled.discover.xdiscover(None, None, 120)
-for controller in controllers:
-    try:
-        controller_interface = xled.ControlInterface(controller.ip_address, controller.hw_address)
-        device_info = controller_interface.get_device_info()
+controllers = xled.discover.xdiscover(None, None, 30)
+
+with suppress(xled.exceptions.DiscoverTimeout):
+    for controller in controllers:
+        controllerlist.append(controller)   
+        print(controllerlist)
+try:
+    for con in controllerlist:
+        control_interface = xled.ControlInterface(con.ip_address, con.hw_address)
+        device_info = control_interface.get_device_info()
         cur.execute("INSERT INTO rxtnet.Riverside(Name, MacAddress, IP, NumLEDS, ChannelsPerLED, StartChannel) VALUES (?, ?, ?, ?, ?, ?)",
-                    (controller.id, controller.hw_address, controller.ip_address, device_info["number_of_led"], len(device_info["led_profile"]), '(0,0)'))
-    except mariadb.Error as e:
-        print(f"Error: {e}")
+                (con.id, con.hw_address, con.ip_address, device_info["number_of_led"], len(device_info["led_profile"]), '(0,0)'))
+        conn.commit()
+except mariadb.Error as e:
+    print(f"Error: {e}")
