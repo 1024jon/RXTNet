9ac16413eeb1754be9446d8a57fa033c1039f4ac
1024jon <1024jon@gmail.com>
2021-03-27 01:56:59 +0000

    i think loop done

--- tests/byteappend.py
+++ tests/byteappend.py
@@ -1,28 +1,20 @@
 universe = 0
-address = 1
-currentbyte = 1
+address = 0
+currentbyte = 0
 number_of_leds =  250
 bytes_per_led = 3
 totalbytes = number_of_leds * bytes_per_led
 
-"""
 while currentbyte <= totalbytes:
-    for b in range(0, bytes_per_led):
-        address += 1
-        currentbyte +=1 
-        print(currentbyte, universe, address)
-        if currentbyte == totalbytes:
-            print("break1")
-            break
+    if address + bytes_per_led <= 512:
+        for b in range(0, bytes_per_led):
+            address += 1
+            currentbyte += 1
+            print(currentbyte, universe, address)
+            if currentbyte == totalbytes:
+                break
+    else:
+        universe += 1
+        address = 0
     if currentbyte == totalbytes:
-        print("break2")
         break
-    universe += 1
-    address = 1
-    """
-    
-while currentbyte <= totalbytes:
-    for b in range(0, bytes_per_led):
-        address += 1
-        currentbyte += 1
-        print(currentbyte, universe, address)
