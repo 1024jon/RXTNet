b8d5882144f0ecf7d7d0e1b8033681504dab3457
1024jon <1024jon@gmail.com>
2021-04-01 23:31:31 +0000

    added burned channels calculation

--- tests/discover_and_add_db.py
+++ tests/discover_and_add_db.py
@@ -39,14 +39,15 @@ try:
         device_info = control_interface.get_device_info()
         if not sel_results:
             curinsert.execute("INSERT INTO rxtnet.Riverside(Name, MacAddress, IP, NumLEDS, ChannelsPerLED, StartChannel, StartUniverse) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                    (con.id, con.hw_address, con.ip_address, device_info["number_of_led"], len(device_info["led_profile"]), '1', '0'))
+                    (con.id, con.hw_address, con.ip_address, device_info["number_of_led"], len(device_info["led_profile"]), '4', '0'))
             conn.commit()
             curinsert.close()
         else:
             startchannel = sel_results[0]
             startuniverse = sel_results[1]
             usedchannels = sel_results[2] * sel_results[3]
-            nextaddr = [startuniverse + (usedchannels//512), startchannel + ((usedchannels%512))]
+            burnedchannels = ((usedchannels//512)-1) + ((512-startchannel+1)%sel_results[3])
+            nextaddr = [startuniverse + (usedchannels//512), startchannel + (usedchannels%512) + burnedchannels]
             curinsert.execute("INSERT INTO rxtnet.Riverside(Name, MacAddress, IP, NumLEDS, ChannelsPerLED, StartChannel, StartUniverse) VALUES (?, ?, ?, ?, ?, ?, ?)",
                     (con.id, con.hw_address, con.ip_address, device_info["number_of_led"], len(device_info["led_profile"]), nextaddr[1], nextaddr[0]))
             conn.commit()
